51b8509b6f07e9cfcae699eabdc2bb9b8ec2aa47
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2021-10-24 16:19:44 +0900

    [add] chapter2 section1 code

---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by くりメガネ on 2021/10/24.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load(filename: "landmarkData.json")
+
+func load<T: Decodable>( filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else{
+        fatalError("Couldn't fine \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
