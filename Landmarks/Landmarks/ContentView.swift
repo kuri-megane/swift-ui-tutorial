3b8e82f99d5f37c6154164f24057e53a44b15ba7
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2021-10-24 14:38:52 +0900

    [add] section1 code

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by くりメガネ on 2021/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! Swift UI")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
